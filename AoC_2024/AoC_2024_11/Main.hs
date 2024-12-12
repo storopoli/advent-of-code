630e4a22240ec609d602e6238ba1071b1027baa3
Jose Storopoli <jose@storopoli.com>
2024-12-12 16:36:29 -0300

    11: part1

--- AoC_2024/AoC_2024_11/Main.hs
+++ AoC_2024/AoC_2024_11/Main.hs
@@ -1,7 +1,46 @@
-
 module Main where
 
-main :: IO()
-main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+import Data.Char (digitToInt)
+import Data.Text qualified (Text)
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
+
+-- Split a number into its left and right halves
+splitNum :: Integer -> (Integer, Integer)
+splitNum n =
+  let s = show n
+      len = length s
+      mid = len `div` 2
+      (left, right) = splitAt mid s
+   in (read left, read right)
+
+-- Check if a number has an even number of digits
+hasEvenDigits :: Integer -> Bool
+hasEvenDigits n = even $ length $ show n
+
+-- Apply transformation rules to a single number
+transform :: Integer -> [Integer]
+transform 0 = [1]
+transform n
+  | hasEvenDigits n = let (l, r) = splitNum n in [l, r]
+  | otherwise = [n * 2024]
+
+-- Apply transformation to all numbers in the list
+step :: [Integer] -> [Integer]
+step = concatMap transform
+
+-- Apply transformation n times
+iterateN :: Int -> [Integer] -> [Integer]
+iterateN 0 xs = xs
+iterateN n xs = iterateN (n - 1) (step xs)
+
+-- Part 1
+part1 :: IO ()
+part1 = do
+  input <- T.lines <$> TIO.readFile "input.txt"
+  let numbers = concatMap (map (read . T.unpack) . T.words) input
+  let result = iterateN 25 numbers
+  print $ length result
+
+main :: IO ()
+main = part1
